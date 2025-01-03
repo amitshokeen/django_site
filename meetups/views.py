26472a84203a07955e1900344d23790e5898eaab
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2025-01-03 18:03:41 +1100

    slug not found handled

--- meetups/views.py
+++ meetups/views.py
@@ -9,9 +9,15 @@ def index(request):
     })
 
 def meetup_details(request, meetup_slug):
-    selected_meetup = Meetup.objects.get(slug=meetup_slug)
-    return render(request, "meetups/meetup-details.html", {
-        'meetup_title': selected_meetup.title,
-        'meetup_description': selected_meetup.description
-    })
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': True,
+            'meetup_title': selected_meetup.title,
+            'meetup_description': selected_meetup.description
+        })
+    except Exception as exc: 
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': False
+        })
 
